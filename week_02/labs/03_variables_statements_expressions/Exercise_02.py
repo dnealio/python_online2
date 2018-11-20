6d28496ad56ae4fd257dff9cf502eeee7f0fd12f
dnealio <djflexneal@gmail.com>
2018-11-19 22:05:11 -0500

    create file using echo command

--- week_02/labs/03_variables_statements_expressions/Exercise_02.py
+++ week_02/labs/03_variables_statements_expressions/Exercise_02.py
@@ -2,5 +2,9 @@
 Write the necessary code to print out the result of the following:
 
 	1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17
-	
+
 '''
+a = 1
+b = 18
+c = sum(i for i in range(a, b) if i%2)
+print(c)

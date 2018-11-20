983be74e4419c53eead88e73683687a381115415
dnealio <djflexneal@gmail.com>
2018-11-19 23:31:39 -0500

    create file using echo command

--- week_02/labs/03_variables_statements_expressions/Exercise_01.py
+++ week_02/labs/03_variables_statements_expressions/Exercise_01.py
@@ -4,3 +4,11 @@ Write the necessary code to print the result of the following formula:
 	(10.5 * 4.2 - 3.4 * 2) / (86.9 - 12.1)
 
 '''
+a = 10.5
+b = 4.2
+c = 3.4
+d = 2
+e = 86.9
+f = 12.1
+print((a*b-c*d)/(e-f))
+

a6d269455cd6cdc077182a42a47b551197f836dd
dnealio <djflexneal@gmail.com>
2018-11-25 22:04:15 -0500

    completed assignment

--- week_02/labs/03_variables_statements_expressions/Exercise_08.py
+++ week_02/labs/03_variables_statements_expressions/Exercise_08.py
@@ -11,3 +11,24 @@ Output should read like - "27.4 degrees celsius = 81.32 degrees fahrenheit"
 NOTE: if you get an error, look up what input() returns!
 
 '''
+
+def fahrenheit(c):
+    f = c*1.8 + 32
+    print(c, "degrees celsius =", f, "degrees fahrenheit")
+fahrenheit(float(input("Input degrees celsius: ")))
+"""
+def fahrenheit(c):
+    f = c*1.8 + 32
+print("input celsius")
+c = input()
+fahrenheit(c)
+"""
+
+"""
+print("Enter temperature in celsius:")
+x = input()
+c = x
+f = (c*1.8 + 32)
+print(f)
+"""
+

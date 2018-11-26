636650248e3f680f0fbc671b181733f5a9fa54f7
dnealio <djflexneal@gmail.com>
2018-11-25 22:41:00 -0500

    completed assignment

--- week_02/labs/04_conditionals_loops/Exercise_02.py
+++ week_02/labs/04_conditionals_loops/Exercise_02.py
@@ -4,3 +4,23 @@ Take in a number from the user and print "Monday", "Tuesday", ...
 or other respectively. Use a "nested-if" statement.
 
 '''
+num = int(input("Enter a number between 1 and 7: "))
+if num >= 1:
+    if num <= 7:
+        if num == 1:
+            print("Monday")
+        if num == 2:
+            print("Tuesday")
+        if num == 3:
+            print("Wednesday")
+        if num == 4:
+            print("Thursday")
+        if num == 5:
+            print("Friday")
+        if num == 6:
+            print("Saturday")
+        if num == 7:
+            print("Sunday")
+    else:
+        print("Other")
+

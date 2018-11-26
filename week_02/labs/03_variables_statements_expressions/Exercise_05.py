75633cc3110ea65346ec343023f8bd063af9578f
dnealio <djflexneal@gmail.com>
2018-11-25 20:07:58 -0500

    create file using echo command

--- week_02/labs/03_variables_statements_expressions/Exercise_05.py
+++ week_02/labs/03_variables_statements_expressions/Exercise_05.py
@@ -3,3 +3,21 @@ Write the necessary code to display the area and perimeter of a rectangle
 that has a width of 2.4 and a height of 6.4
 
 '''
+"""
+w = 2.4
+h = 6.4
+"""
+
+def perimeter (w, h):
+    width = 2*w
+    height = 2*h
+    result = (width + height)
+    print(result)
+perimeter(2.4, 6.4)
+
+def area (w, h):
+    width = w
+    height = h
+    result = (width*height)
+    print(result)
+area(2.4, 6.4)

3b5b933e83417f4f7bd4bead838351d25a5f74a0
dnealio <djflexneal@gmail.com>
2018-11-25 21:59:09 -0500

    completed assignment

--- week_02/labs/03_variables_statements_expressions/Exercise_09.py
+++ week_02/labs/03_variables_statements_expressions/Exercise_09.py
@@ -7,3 +7,21 @@ Receive the following arguments from the user:
 Display the cost of the trip in the console.
 
 '''
+"""
+print("How many miles are you driving?")
+m = input()
+print("what is the MPG of the car?")
+mpg = input()
+print("How much does fule cost?")
+f = input()
+
+cost = (m/mpg)*(f)
+print(cost)
+
+"""
+def cost(a,b,c):
+    result = (a/b)*c
+    print("The cost of the trip in dollars is:",result)
+cost(a=int(input("enter the miles to drive: ")),
+    b=int(input("enter the mpg of the car: ")),
+    c=int(input("enter the price per gallon of fuel: ")))

e2ae7558addf9b791ff62e32670e031eb1289ae1
dnealio <djflexneal@gmail.com>
2018-11-25 20:18:57 -0500

    create file using echo command

--- week_02/labs/03_variables_statements_expressions/Exercise_06.py
+++ week_02/labs/03_variables_statements_expressions/Exercise_06.py
@@ -3,3 +3,9 @@ If a runner runs 12 kilometers in 30 minutes and 30 seconds.
 What is his/her average speed in miles per hour. (Tip: 1 mile = 1.6 km)
 
 '''
+def speed(k, h, m, s):
+    mile = k/1.6
+    time = (h + m/60 + s/3600)
+    result = (mile/time)
+    print(result)
+speed(12, 0, 30, 30)

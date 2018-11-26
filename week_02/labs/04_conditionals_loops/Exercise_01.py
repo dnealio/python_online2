2241475c017478f77f658921b948345b387c0cc0
dnealio <djflexneal@gmail.com>
2018-11-25 22:17:25 -0500

    completed assignment

--- week_02/labs/04_conditionals_loops/Exercise_01.py
+++ week_02/labs/04_conditionals_loops/Exercise_01.py
@@ -6,3 +6,9 @@ Print the result.
 NOTE: We will be using the input() function. This is demonstrated below.
 
 '''
+def number(n):
+    if n%2 == 0:
+        print("even")
+    else:
+        print("odd")
+number(n=int(input("Enter a number between 1 and 1,000,000,000: ")))

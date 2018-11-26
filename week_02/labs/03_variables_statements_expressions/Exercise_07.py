bcf82dc44bb65eb5cf4dae03441e0713aef19eaa
dnealio <djflexneal@gmail.com>
2018-11-25 20:33:38 -0500

    completed assignment

--- week_02/labs/03_variables_statements_expressions/Exercise_07.py
+++ week_02/labs/03_variables_statements_expressions/Exercise_07.py
@@ -9,3 +9,11 @@ for the next 3 years (without a leap year).
 Let's say the current population is 380,123,456.
 
 '''
+def population(b, d, i, c, y):
+    seconds = y*365*24*60*60
+    births = seconds/b
+    deaths = seconds/d
+    immigrants = seconds/i
+    result = (c+births+immigrants-deaths)
+    print(result)
+population(6,12,40,380123456,3)

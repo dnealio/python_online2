299c4e67d75ac64783e807676955a4c8b90533c2
dnealio <djflexneal@gmail.com>
2018-11-27 22:03:32 -0500

    completed assignment

--- week_02/labs/04_conditionals_loops/Exercise_04.py
+++ week_02/labs/04_conditionals_loops/Exercise_04.py
@@ -2,3 +2,15 @@
 Print out every prime number between 1 and 100.
 
 '''
+a = 1
+b = 100
+
+
+for x in range(a, b+1):
+    if x>1:
+        for i in range(2, x):
+            if(x%i) == 0:
+                break
+        else:
+            print(x)
+

a19e0897364dfb60ff554d10937ae3cd39081e3a
dnealio <djflexneal@gmail.com>
2018-11-27 21:37:07 -0500

    completed assignment

--- week_02/labs/04_conditionals_loops/Exercise_03.py
+++ week_02/labs/04_conditionals_loops/Exercise_03.py
@@ -2,3 +2,17 @@
 Using a "for-loop", print out all even numbers from 1-100.
 
 '''
+"""
+a = 0
+b = 100
+
+for x in range(a, b+1, 2):
+    print(x)
+
+"""
+a = 0
+b = 100
+
+for x in range(a, b+1):
+    if x%2 == 0:
+        print(x)

b53bda26c6f0db525ca7a5e0b362187ed239d493
dnealio <djflexneal@gmail.com>
2018-12-01 17:27:36 -0500

    sum and average between bounds

--- week_02/labs/04_conditionals_loops/Exercise_05.py
+++ week_02/labs/04_conditionals_loops/Exercise_05.py
@@ -7,3 +7,50 @@ Take two numbers from the user, an upper and lower bound. Using a loop, calculat
 		The sum is: 5050
 		The average is: 50.5
 '''
+
+#First we need a lower bound using the input function
+#We need an upper bound using the input function
+#We need to find all the numbers between bounds and loop through them
+#Use a for loop to loop through the user input bounds
+#set the sum of this loop to = result
+#print result
+#calculate amount of numbers in the range by subtracting the lower bound from upper bound
+#find average using this # and the result variable
+
+
+print("Let's calculate the sum and average of the numbers between two bounds")
+
+
+l = int(input("Lower Bound =: "))
+u = int(input("Upper Bound =: "))
+
+result = sum(x for x in range(l, u))
+print("The sum of all numbers between", l, "and", u, "=", result)
+
+average = float(result / (u-l))
+print("The average =", average)
+
+
+"""
+for x in range(l, u):
+    result == sum(x)
+    print(result)
+"""
+
+
+"""
+    for i in range(l, x):
+        if(x%l) == 1:
+            break
+        else:
+            print(x)
+"""
+
+"""
+L = int(input("Input lower bound: "))
+U = int(input("Input upper bound: "))
+
+for x in range(L, U):
+    result = sum(x)
+    print(result)
+"""

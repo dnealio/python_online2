e67820c4e289d269d228b92e70fa7917904d2946
dnealio <djflexneal@gmail.com>
2018-12-02 12:47:44 -0500

    square of numbers in a user defined range, two approaches

--- week_02/labs/04_conditionals_loops/Exercise_10.py
+++ week_02/labs/04_conditionals_loops/Exercise_10.py
@@ -5,3 +5,24 @@ from a user inputed lower to a user inputed upper bound.
 Use a for loop that demonstrates the use of the range function.
 
 '''
+
+#initial approach
+"""
+import math
+
+a = int(input("lower bound: "))
+b = int(input("upper bound: "))
+
+for x in range(a,b+1):
+    print(x**2)
+"""
+
+#consolidated approach
+
+print("Do you want to know what the square root is for a range of numbers?")
+print("enter a lower and upper bound separated by a space")
+
+a, b = input().split()
+
+for x in range(int(a), int(b)+1):
+    print("The square of", x, "is", x**2)

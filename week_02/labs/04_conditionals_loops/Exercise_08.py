ae9659ed44bad45f5fec33e38ea3146338fb2133
dnealio <djflexneal@gmail.com>
2018-12-02 12:13:20 -0500

    Using break to exit a loop

--- week_02/labs/04_conditionals_loops/Exercise_08.py
+++ week_02/labs/04_conditionals_loops/Exercise_08.py
@@ -2,3 +2,18 @@
 Demonstrate the use of the "break" statement to exit a loop.
 
 '''
+
+
+#Counts back from user input every 3 numbers until the sum of the numbers exceeds 500
+
+print("This script will count backwards every three numbers from the number you input, and sum all the numbers until the sum is greater than 500")
+a = int(input("Enter number: "))
+_sum = 0
+
+while(a>=1):
+    print(a)
+    _sum=_sum+a
+    a+=(-3)
+    if _sum >= 500:
+        break
+print("sum is", _sum)
